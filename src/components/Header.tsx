1c7ce44c01a0bef316aee69acfecc170f02e8509
Emmanuel Holonou <manelhln00@gmail.com>
2024-04-03 12:03:02 +0100

    Updated header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,7 +11,7 @@ export default function Header() {
       <div>
         <div className="flex justify-between mb-2">
           <Link href="/">
-          <img src="/images/logo.svg" alt="" />
+            <img src="/images/logo.svg" alt="" />
           </Link>
           <div>
             <img
@@ -37,7 +37,7 @@ export default function Header() {
         <motion.div
           whileInView={{ x: [300, 0] }}
           transition={{ duration: 0.55, ease: "easeOut" }}
-          className={`flex flex-col items-center min-h-screen fixed top-0 bottom-0 left-0 w-full max-w-full z-10 bg-[#BE894A] p-4`}
+          className={`flex flex-col items-center justify-between min-h-screen fixed top-0 bottom-0 left-0 w-full max-w-full z-10 bg-[#BE894A] p-4`}
         >
           <div className="absolute top-8 right-8">
             <img
@@ -50,19 +50,34 @@ export default function Header() {
             <li
               className={`uppercase text-white font-semibold text-2xl leading-normal tracking-widest ${poppins.className}`}
             >
-              <Link href="/le-projet" onClick={()=>setIsMobileMenuopen(false)}>le projet miduwe</Link>
+              <Link
+                href="/le-projet"
+                onClick={() => setIsMobileMenuopen(false)}
+              >
+                le projet miduwe
+              </Link>
             </li>
             <li
               className={`uppercase text-white font-semibold text-2xl leading-normal tracking-widest ${poppins.className}`}
             >
-              <Link href="/a-propos" onClick={()=>setIsMobileMenuopen(false)}>qui sommes nous?</Link>
+              <Link href="/a-propos" onClick={() => setIsMobileMenuopen(false)}>
+                qui sommes nous?
+              </Link>
             </li>
             <li
               className={`uppercase text-white font-semibold text-2xl leading-normal tracking-widest ${poppins.className}`}
             >
-              <Link href="/contact" onClick={()=>setIsMobileMenuopen(false)}>Contact</Link>
+              <Link href="/contact" onClick={() => setIsMobileMenuopen(false)}>
+                Contact
+              </Link>
             </li>
           </ul>
+
+          <button className="bg-[#3F0408] rounded-full px-[60px] py-5 text-[#BE894A] text-xl mb-10">
+            <Link href="/play" onClick={() => setIsMobileMenuopen(false)}>
+              Acceder au jeu
+            </Link>
+          </button>
         </motion.div>
       )}
     </div>
